b36a753198a0d0189539c33bd1fb488bc9dcf0a3
ZoeDominguez <zdominguezcarballo@frba.utn.edu.ar>
2024-04-14 20:41:47 +0000

    creo servidor de memoria

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,8 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
+#include <utils/server.h>
+#include <utils/inicio.h>
 
-int main(int argc, char* argv[]) {
-    decir_hola("Memoria");
+int main(void) {
+    t_log* memoria_log;
+	t_config* memoria_config;
+    char* puerto;
+    int memoria_server;
+
+    memoria_log = iniciar_logger("memoria.log","memoria");
+    memoria_config = iniciar_config("memoria.config");
+    puerto = config_get_string_value(puerto ,"PUERTO_ESCUCHA");
+    memoria_server = iniciar_servidor(puerto);
+    log_info(memoria_log, "Modulo memoria lista para recibir a kernel");
+    esperar_cliente(memoria_server);
+
     return 0;
 }
