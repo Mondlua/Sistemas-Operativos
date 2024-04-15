b5669cfdf86c25354fe3cd830a3a40f56e15e023
Emilia Andersen <129566261+EmiliaAndersen@users.noreply.github.com>
2024-04-14 23:19:19 -0300

    conexiones kernel

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,29 +7,53 @@ int main(void) {
     int conexion_cpu_dispatch;
     int conexion_cpu_interrupt;
     int conexion_memoria;
+    int quantum;
+    // char* recursos[];
+    // char* instancias_recursos[];
+    int grado_multiprogramacion;
     char* ip_memoria;
     char* ip_cpu;
     char* puerto_memoria;
     char* puerto_cpu_dispatch;
     char* puerto_cpu_interrupt;
 	char* puerto_escucha;
+    char* algoritmo_planificacion;
     t_log* kernel_log;
 	t_config* kernel_config;
 
     kernel_log = iniciar_logger("kernel.log","kernel");
     kernel_config = iniciar_config("kernel.config");
-    //log_info(kernel_log, "hola");
-	
-	ip_memoria = config_get_string_value(kernel_config,"IP_MEMORIA");
-	ip_cpu = config_get_string_value(kernel_config, "IP_CPU");
+    
+	/*
+	//ip_memoria = config_get_string_value(kernel_config,"IP_MEMORIA");
+	//ip_cpu = config_get_string_value(kernel_config, "IP_CPU");
 	puerto_cpu_dispatch = config_get_string_value(kernel_config, "PUERTO_CPU_DISPATCH");
 	puerto_cpu_interrupt = config_get_string_value(kernel_config, "PUERTO_CPU_INTERRUPT");
 	puerto_memoria = config_get_string_value(kernel_config, "PUERTO_MEMORIA");
 	puerto_escucha = config_get_string_value(kernel_config, "PUERTO_ESCUCHA");
 
-	conexion_memoria = crear_conexion(ip_memoria, puerto_memoria);
 
+	conexion_memoria = crear_conexion(ip_memoria, puerto_memoria);
+*/
+//////// servidor kernel con i/o
+  /*  char* puerto;
+    int kernel_server;
+
+    puerto = config_get_string_value(kernel_config ,"PUERTO_ESCUCHA");
+    kernel_server = iniciar_servidor(puerto, kernel_log);
+    log_info(kernel_log, "Modulo kernel lista para recibir a i/o");
+    esperar_cliente(kernel_server, kernel_log);
+*/
+//cliente kernel con cpu
+    ip_cpu = config_get_string_value(kernel_config,"IP_CPU");
+	puerto_cpu_dispatch = config_get_string_value(kernel_config, "PUERTO_CPU_DISPATCH");
+	puerto_cpu_interrupt = config_get_string_value(kernel_config, "PUERTO_CPU_INTERRUPT");
+	puerto_escucha = config_get_string_value(kernel_config, "PUERTO_ESCUCHA");
+
+    conexion_cpu_dispatch = crear_conexion(ip_cpu,puerto_cpu_dispatch);
+ //   conexion_memoria = crear_conexion(ip_cpu,puerto_cpu_interrupt);
 
     return 0;
 }
+
 
