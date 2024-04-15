9260cd37e1b93ad72438750a15657311769344d7
Emilia Andersen <129566261+EmiliaAndersen@users.noreply.github.com>
2024-04-14 23:18:18 -0300

    conexiones entradasalida

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -1,8 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
+#include <utils/client.h>
+#include <utils/inicio.h>
 
-int main(int argc, char* argv[]) {
-    decir_hola("una Interfaz de Entrada/Salida");
+int main(void) {
+    int conexion_memoria;
+    char* ip_memoria;
+    char* puerto_memoria;
+
+    char* ip_kernel;
+    char* puerto_kernel;
+    int conexion_kernel;
+
+
+	char* puerto_escucha;
+    t_log* entradasalida_log;
+	t_config* entradasalida_config;
+
+    entradasalida_log = iniciar_logger("entradasalida.log","entradasalida");
+    entradasalida_config = iniciar_config("entradasalida.config");
+    
+	// cliente i/o con kernel
+	ip_memoria = config_get_string_value(entradasalida_config,"IP_MEMORIA");
+	puerto_memoria = config_get_string_value(entradasalida_config, "PUERTO_MEMORIA");
+
+    ip_kernel= config_get_string_value(entradasalida_config,"IP_KERNEL");
+    puerto_kernel = config_get_string_value(entradasalida_config, "PUERTO_KERNEL");
+
+	puerto_escucha = config_get_string_value(entradasalida_config, "PUERTO_ESCUCHA");
+
+	// conexion_memoria = crear_conexion(ip_memoria, puerto_memoria);
+    
+    conexion_kernel = crear_conexion(ip_kernel, puerto_kernel);
+
+
     return 0;
 }
+
