9f612ba18fd65bee069012159504d376bf37a5bf
ZoeDominguez <zdominguezcarballo@frba.utn.edu.ar>
2024-04-17 17:12:19 +0000

    Agrego archivos de mensajes, ya no hay error de superposicion de datos

--- utils/src/utils/comunicacion.h
+++ utils/src/utils/comunicacion.h
@@ -1,5 +1,5 @@
-#ifndef COMUNICACION_Mem_H_
-#define COMUNICACION_Mem_H_
+#ifndef COMUNICACION_H_
+#define COMUNICACION_H_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,6 +8,16 @@
 #include <inttypes.h>
 #include <commons/log.h>
 #include <pthread.h>
+
+#include <utils/mensajes.h>
+
+typedef struct
+{
+    t_log *log;
+    int c_socket;
+    char *server_name;
+
+} t_atender_cliente_args;
 
 int server_escuchar(t_log* logger, char* server_name, int server_socket);
 
--- utils/src/utils/server.h
+++ utils/src/utils/server.h
@@ -1,5 +1,5 @@
-#ifndef UTILS_H_
-#define UTILS_H_
+#ifndef UTILS_SERVER_H_
+#define UTILS_SERVER_H_
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -7,30 +7,12 @@
 #include<unistd.h>
 #include<netdb.h>
 #include<commons/log.h>
-#include<commons/collections/list.h>
 #include<string.h>
 #include<assert.h>
-#include<commons/config.h>
 
 
-
-typedef enum
-{
-    MENSAJE,
-    PAQUETE,
-//?
-    CREAR_PROCESO,
-    FINALIZAR_PROCESO,
-    ACCEDER_TABLAS_P,
-    AJUSTAR_TAMANIO,
-    ACCEDER_ESPACIO_U
-}op_code;
-
-void* recibir_buffer(int*, int);
 int iniciar_servidor(char*, t_log*);
 int esperar_cliente(int, t_log*);
-t_list* recibir_paquete(int);
-void recibir_mensaje(int, t_log*);
-int recibir_operacion(int);
+
 
 #endif
