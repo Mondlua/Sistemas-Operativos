b084e48950a34d6ad23b5082dac0a2f5c3ebcc3d
EmiliaAndersen <mandersen@frba.utn.edu.ar>
2024-04-19 15:42:07 +0000

    Ordeno mains

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -4,20 +4,30 @@
 #include <utils/comunicacion.h>
 
 int main(void) {
+
     t_log* memoria_log;
 	t_config* memoria_config;
+
     char* puerto;
     int memoria_server;
-    
- /*                     SERVER                         */
-    memoria_log = iniciar_logger("memoria.log","Memoria");
+
+
+    memoria_log = iniciar_logger("memoria.log","memoria");
     memoria_config = iniciar_config("memoria.config");
+
+
+    /* MEMORIA - Server */
+
+    // Extraer configs
+
     puerto = config_get_string_value(memoria_config ,"PUERTO_ESCUCHA");
+
+    // Inicio server 
+
     memoria_server = iniciar_servidor(puerto, memoria_log);
-    log_info(memoria_log, "Modulo Memoria lista para recibir clientes");
-    server_escuchar(memoria_log, "Memoria", memoria_server);
+    log_info(memoria_log, "MEMORIA lista para recibir clientes");
+    server_escuchar(memoria_log, "memoria", memoria_server);
 
     return 0;
-
 }
 
