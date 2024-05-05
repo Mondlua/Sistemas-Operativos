2d7eb95e3682a5388fd8dc1ba7d10ade5aeafa17
Mondlua <zoe.camiluna@gmail.com>
2024-05-05 22:53:45 +0000

    pepe recibe mensajes T-T

--- utils/src/utils/mensajes.h
+++ utils/src/utils/mensajes.h
@@ -9,13 +9,14 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
 
 typedef enum
 {
     MENSAJE,
     PAQUETE,
     INTERFAZ,
-    AVISO_DESCONEXION,
+    AVISO_DESCONEXION
 }op_code;
 
 
@@ -31,8 +32,44 @@ typedef struct
     t_buffer* buffer;
 } t_paquete;
 
-void aviso_desconexion(char* , int);
 
+typedef struct {
+    uint32_t size; // Tamaño del payload
+    uint32_t offset; // Desplazamiento dentro del payload
+    void* stream; // Payload
+} t_buffer_ins;
+typedef enum
+{
+    IO_GEN_SLEEP,
+    IO_STDIN_READ,
+    IO_STDOUT_WRITE,
+    IO_FS_CREATE,
+    IO_FS_DELETE,
+    IO_FS_TRUNCATE,
+    IO_FS_WRITE,
+    IO_FS_READ
+} IO_OPERATION;
+
+typedef struct
+{
+    IO_OPERATION codigo_operacion;
+    t_buffer_ins* buffer;
+} t_paquete_instruccion;
+
+typedef struct 
+{   
+     union {
+        struct {
+            char* unidades_trabajo;
+        } io_gen_sleep_params;
+         struct {
+            char* nombre_archivo;
+        } io_fs_create_params;
+     }params;
+}instruccion_params;
+
+
+void aviso_desconexion(char* , int);
 void* recibir_buffer(int*, int);
 t_list* recibir_paquete(int);
 void recibir_mensaje(int, t_log*);
@@ -47,5 +84,10 @@ char* recibir_interfaz(int socket_cliente, t_log* logger);
 bool rcv_handshake(int fd_conexion);
 bool send_handshake(int conexion, t_log* logger, const char* conexion_name);
 char* recibir_desconexion(int socket_cliente, t_log* logger);
+
+t_buffer_ins* serializar_io_gen_sleep(instruccion_params* param);
+instruccion_params* deserializar_io_gen_sleep(t_buffer_ins* buffer);
+void enviar_instruccion(t_paquete_instruccion* , instruccion_params* , int );
+instruccion_params* recibir_instruccion(int socket_servidor);
 
 #endif
