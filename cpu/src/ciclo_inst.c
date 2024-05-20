562cdadd21f5053dc9887280a2e51d0f6f6c0b31
camisolimano <csolimano@frba.utn.edu.ar>
2024-05-20 20:56:53 +0000

    empezamos decode Co-authored-by: Emilia Andersen <EmiliaAndersen@users.noreply.github.com>

--- cpu/src/ciclo_inst.c
+++ cpu/src/ciclo_inst.c
@@ -1,21 +1,4 @@
 #include "ciclo_inst.h"
-/*
-void inicializar_registro(t_pcb* pcb)
-{
-    cpu_registros *registros = pcb->registros;
-    registros= malloc(sizeof(cpu_registros));
-    registros->PC = malloc(sizeof(4));
-    registros->AX = malloc(sizeof(1));
-    registros->BX = malloc(sizeof(1));
-    registros->CX = malloc(sizeof(1));
-    registros->DX = malloc(sizeof(1));
-    registros->EAX = malloc(sizeof(4));
-    registros->EBX = malloc(sizeof(4));
-    registros->ECX = malloc(sizeof(4));
-    registros->EDX = malloc(sizeof(4));
-    registros->SI = malloc(sizeof(4));
-    registros->DI = malloc(sizeof(4));
-}*/
 
 t_instruccion* fetch(t_pcb* pcb, int conexion_memoria){
     
@@ -28,4 +11,145 @@ t_instruccion* fetch(t_pcb* pcb, int conexion_memoria){
     
     pcb->p_counter++;
     return instruccion;
+}
+
+t_decode* decode(t_instruccion* instruccion){
+
+    instrucciones ins;
+    char* buffer = (char*) instruccion->buffer->stream;
+    char** arrayIns = malloc(sizeof(char*)*4);
+    arrayIns = string_split(buffer," ");
+
+    ins = obtener_instruccion(arrayIns[0]);
+    t_decode* decode;
+    decode->op_code = ins;
+    decode->registroCpu = list_create();
+    
+    switch(ins){
+        case 0:{
+        cpu_registros* registro = arrayIns[1];
+        list_add(decode->registroCpu, registro);
+        int valor =atoi(arrayIns[2]);
+        decode->valor = valor;          
+        }
+        case 1:{
+        cpu_registros* registroDatos = arrayIns[1];
+        list_add(decode->registroCpu,registroDatos);
+        cpu_registros* registroDireccion = arrayIns[2];
+        list_add(decode->registroCpu,registroDireccion);     
+        }
+        case 2:{
+        cpu_registros* registroDireccion = arrayIns[1];
+        list_add(decode->registroCpu,registroDireccion);
+        cpu_registros* registroDatos = arrayIns[2];
+        list_add(decode->registroCpu,registroDatos);   
+        decode->logicaAFisica= true;  
+        }
+        case 3:{
+        cpu_registros* registroDestino = arrayIns[1];
+        list_add(decode->registroCpu,registroDestino);
+        cpu_registros* registroOrigen= arrayIns[2];
+        list_add(decode->registroCpu,registroOrigen);     
+        }
+        case 4:{
+        cpu_registros* registroDestino = arrayIns[1];
+        list_add(decode->registroCpu,registroDestino);
+        cpu_registros* registroOrigen= arrayIns[2];
+        list_add(decode->registroCpu,registroOrigen);
+        }
+        case 5:{
+        instrucciones ins = obtener_instruccion(arrayIns[1]);
+        decode->ins= ins;
+        }
+        case 6:{
+        int tamanio = atoi(arrayIns[1]);
+        decode->valor= tamanio;
+        }   
+        case 7:{
+        int tamanio = atoi(arrayIns[1]);
+        decode->valor= tamanio;
+        }
+        case 8:{
+        char *rec;
+        rec= arrayIns[1];
+        decode->recurso = rec;
+        }
+        case 9:{
+        char* recu = arrayIns[1];
+        decode->recurso=recu;
+        }
+        case 10:{
+
+        }
+        case 11:{
+            
+        }
+        case 12:{
+            
+        }
+        case 13:{
+            
+        }
+        case 14:{
+            
+        }
+        case 15:{
+            
+        }
+        case 16:{
+            
+        }
+        case 17:{
+            
+        }
+        case 18:{
+            
+        }
+    }
+   
+   return decode;
+}
+
+instrucciones obtener_instruccion(char *nombre) {
+    if (strcmp(nombre, "SET") == 0) {
+        return SET;
+    } else if (strcmp(nombre, "MOV_IN") == 0) {
+        return MOV_IN;
+    } else if (strcmp(nombre, "MOV_OUT") == 0) {
+        return MOV_OUT;
+    } else if (strcmp(nombre, "SUM") == 0) {
+        return SUM;
+    } else if (strcmp(nombre, "SUB") == 0) {
+        return SUB;
+    } else if (strcmp(nombre, "JNZ") == 0) {
+        return JNZ;
+    } else if (strcmp(nombre, "RESIZE") == 0) {
+        return RESIZE;
+    } else if (strcmp(nombre, "COPY_STRING") == 0) {
+        return COPY_STRING;
+    } else if (strcmp(nombre, "WAIT") == 0) {
+        return WAIT;
+    } else if (strcmp(nombre, "SIGNAL") == 0) {
+        return SIGNAL;
+    } else if (strcmp(nombre, "IO_GEN_SLEEP") == 0) {
+        return IO_GEN_SLEEP;
+    } else if (strcmp(nombre, "IO_STDIN_READ") == 0) {
+        return IO_STDIN_READ;
+    } else if (strcmp(nombre, "IO_STDOUT_WRITE") == 0) {
+        return IO_STDOUT_WRITE;
+    } else if (strcmp(nombre, "IO_FS_CREATE") == 0) {
+        return IO_FS_CREATE;
+    } else if (strcmp(nombre, "IO_FS_DELETE") == 0) {
+        return IO_FS_DELETE;
+    } else if (strcmp(nombre, "IO_FS_TRUNCATE") == 0) {
+        return IO_FS_TRUNCATE;
+    } else if (strcmp(nombre, "IO_FS_WRITE") == 0) {
+        return IO_FS_WRITE;
+    } else if (strcmp(nombre, "IO_FS_READ") == 0) {
+        return IO_FS_READ;
+    } else if (strcmp(nombre, "EXIIT") == 0) {
+        return EXIIT;
+    } else {
+        return -1;
+    }
 }
--- memoria/src/memoriaserver.c
+++ memoria/src/memoriaserver.c
@@ -43,10 +43,10 @@ void atender_cliente(void *void_args)
         {
             char * pc_recibido = recibir_pc(client_socket);
             int pc = atoi(pc_recibido);
-
-            /*t_instruccion* instruccion = list_get(lista_arch,pc);
-            
-            enviar_instruccion_mem(client_socket,instruccion);*/
+            log_info(logger, "Mando la pc %d", pc);
+            t_instruccion* instruccion = list_get(lista_arch,pc);
+            log_info(logger, "Mando la INSTRUCCION %s", instruccion->buffer->stream);
+            enviar_instruccion_mem(client_socket,instruccion);
            
             break;
         }
