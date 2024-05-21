eae0d4153d2fd61b015a8a730e80833db72bb9a1
EmiliaAndersen <mandersen@frba.utn.edu.ar>
2024-05-21 19:10:48 +0000

    sum
    Co-authored-by: camisolimano <csolimano@frba.utn.edu.ar>

--- kernel/src/utils_consola.c
+++ kernel/src/utils_consola.c
@@ -15,12 +15,16 @@ t_pcb *crear_nuevo_pcb(uint32_t *pid_contador)
     nuevo_pcb->p_counter = 0; 
     nuevo_pcb->quantum = config_get_int_value(kernel_config, "QUANTUM");
     inicializar_registro(nuevo_pcb);
+    uint8_t x = 1;
+    uint8_t y = 2;
+    nuevo_pcb->registros->AX = x;
+    nuevo_pcb->registros->BX = y;
     nuevo_pcb->tabla_paginas = NULL;
     nuevo_pcb->algoritmo_planif = config_get_string_value(kernel_config, "ALGORITMO_PLANIFICACION");
     nuevo_pcb->estado = NEW;
 
     log_info(kernel_log, "Se crea el proceso con PID = %u en NEW", nuevo_pcb->pid);
-
+    log_info(kernel_log, "Se crea el proceso con ax = %u en NEW", nuevo_pcb->registros->AX);
     (*pid_contador)++;
     
     return nuevo_pcb;
@@ -172,19 +176,19 @@ void enviar_pcb_cpu(t_pcb* pcb, int socket_cliente){
 
 void inicializar_registro(t_pcb* pcb)
 {
-    cpu_registros *registros = pcb->registros;
-    registros= malloc(sizeof(cpu_registros));
-    registros->PC = malloc(sizeof(uint32_t));
-    registros->AX = malloc(sizeof(uint8_t));
-    registros->BX = malloc(sizeof(uint8_t));
-    registros->CX = malloc(sizeof(uint8_t));
-    registros->DX = malloc(sizeof(uint8_t));
-    registros->EAX = malloc(sizeof(uint32_t));
-    registros->EBX = malloc(sizeof(uint32_t));
-    registros->ECX = malloc(sizeof(uint32_t));
-    registros->EDX = malloc(sizeof(uint32_t));
-    registros->SI = malloc(sizeof(uint32_t));
-    registros->DI = malloc(sizeof(uint32_t));
+
+    pcb->registros= malloc(sizeof(cpu_registros));
+    pcb->registros->PC = malloc(sizeof(uint32_t));
+    pcb->registros->AX = malloc(sizeof(uint8_t));
+    pcb->registros->BX = malloc(sizeof(uint8_t));
+    pcb->registros->CX = malloc(sizeof(uint8_t));
+    pcb->registros->DX = malloc(sizeof(uint8_t));
+    pcb->registros->EAX = malloc(sizeof(uint32_t));
+    pcb->registros->EBX = malloc(sizeof(uint32_t));
+    pcb->registros->ECX = malloc(sizeof(uint32_t));
+    pcb->registros->EDX = malloc(sizeof(uint32_t));
+    pcb->registros->SI = malloc(sizeof(uint32_t));
+    pcb->registros->DI = malloc(sizeof(uint32_t));
 }
 
 /*void cambiar_cola(t_pcb* pcb,t_proceso_estado nuevo_estado){
