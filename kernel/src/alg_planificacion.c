02494842f22964dc0004148cf082309289fa5725
FrancoMagnaghi <fmagnaghi@frba.utn.edu.ar>
2024-05-22 22:25:51 +0000

    bloqueos por recursos

--- kernel/src/alg_planificacion.c
+++ kernel/src/alg_planificacion.c
@@ -1,21 +1,23 @@
 #include "alg_planificacion.h"
 
-typedef enum t_motivos_desalojo
+typedef enum t_motivos_desalojo // Los comparten el Kernel y el CPU
 {
     INS_EXIT,
-    BLOCK_IO
-    //BLOCK_RECURSO
+    BLOCK_IO,
+    BLOCK_RECURSO
 
 } t_motivos_desalojo;
 
 void fifo(int conexion_cpu_dispatch){
-   int tamanioExec = queue_size(colaExec);
    int tamanioReady = queue_size(colaReady);
    int motivo_desalojo;
    t_pcb* pcb_actualizado;
    t_pcb* pcb_a_planificar;
+   t_instruccion* instruccion;
+   char* instruccion_string;
+   char** array_palabras;
 
-    if(tamanioExec == 0 && tamanioReady > 0){
+    if(tamanioReady > 0){
         while( tamanioReady>0 ){ //Cuando se implimente el comando "DETENER_PLANIFICACION", validar tambien que la planif no haya sido pausada
             pcb_a_planificar = queue_pop(colaReady);
             pcb_a_planificar->estado=EXEC;
@@ -27,51 +29,72 @@ void fifo(int conexion_cpu_dispatch){
             queue_pop(colaExec);
             switch (motivo_desalojo) {
                 case INS_EXIT:
-                    pcb_actualizado->estado=EXIT;
-                    queue_push(colaExit, pcb_actualizado);
+                    cambiar_a_exit(pcb_actualizado);
                     break;
                 case BLOCK_IO:
-                    t_instruccion* instruccion = recibir_instruccion_io(conexion_memoria);
-                    char* instruccion_string = instruccion->buffer->stream;
-                    char** array_palabras = string_split(instruccion_string, " ");
+                    instruccion = recibir_instruccion_memoria(conexion_memoria);
+                    instruccion_string = instruccion->buffer->stream;
+                    array_palabras = string_split(instruccion_string, " ");
 
                     char* instruccionIO = array_palabras[0];
                     char* interfazIO = array_palabras[1];
 
                 if(!validar_conexion_IO(interfazIO)){ // Si no existe la interfaz o no esta conectada , el proceso pasa a EXIT
-                    pcb_actualizado->estado=EXIT;
-                    queue_push(colaExit, pcb_actualizado);
+                    cambiar_a_exit(pcb_actualizado);
                     break;
                 } else if(!validar_operacion(tipoInterfaz,instruccion)){ // Si no puede realizar la operacion solicitada, el proceso pasa a EXIT
-                    pcb_actualizado->estado=EXIT;
-                    queue_push(colaExit, pcb_actualizado);
+                    cambiar_a_exit(pcb_actualizado);
                     break;
-                } else if(interfaz_esta_libre()){ // Creo que aca se podria usar "validar_peticion(interfaz, tiempo)" pero es void
+                } else if(interfaz_esta_libre()){
                     pcb_actualizado->estado=BLOCKED;
-                    manejar_operacion_io(instruccion);
-                    pcb_actualizado->estado=READY;
-                    queue_push(colaReady, pcb_actualizado);
+                    queue_push(colaBlocked, pcb_actualizado);
+                    manejar_operacion_io(instruccion); // Un hilo para manejar las operaciones IO
                     break;
                 }
+                    break;
+                case BLOCK_RECURSO:
+                    instruccion = recibir_instruccion_memoria(conexion_memoria);
+                    instruccion_string = instruccion->buffer->stream;
+                    array_palabras = string_split(instruccion_string, " ");
 
+                    char* instruccionRecurso = array_palabras[0];
+                    char* recurso = array_palabras[1];
+
+                if(!validar_recurso(recurso)){
+                    cambiar_a_exit(pcb_actualizado);
                 }
+                    if(strcmp(instruccionRecurso, "WAIT")){
+                        instanciasDisponibles = list_get(listaInstanciasRecursos, indiceRecurso);
+                        instanciasDisponibles--;
+                        if(instanciasDisponibles<0){
+                            pcb_actualizado->estado=BLOCKED;
+                            queue_push(colaBlocked, pcb_actualizado);
+                            break;
+                        }else{break;} 
+                    }else if(strcmp(instruccionRecurso, "SIGNAL")){
+                        instanciasDisponibles = list_get(listaInstanciasRecursos, indiceRecurso);
+                        instanciasDisponibles++;
+                        if(instanciasDisponibles>0){
+                            //sacar de la cola de blocked del recurso
+                            break;    
+                    }else{break;} 
                     break;
-                /*case BLOCK_RECURSO:
+                }
                 
-                default:*/ // (*) Ver enunciado, ¿el estado BLOCKED tiene "multiples colas" una por cada IO y una por cada recurso?
+                
+                
+                default:
             }
 
-            tamanioExec = queue_size(colaExec);
             tamanioReady = queue_size(colaReady);
         }
    }
 
 
    //---------------------------------
-  t_instruccion* recibir_instruccion_io(int socket_conexion){
-
+  t_instruccion* recibir_instruccion_memoria(int socket_conexion){
     enviar_pc(int_to_char(pcb_a_planificar->p_counter),conexion_memoria);
-    t_instruccion* instruccion = recibir_instruccion_de_memoria(conexion_memoria); // esta funcion tiene el nombre de recibir_instruccion_cpu
+    t_instruccion* instruccion = recibir_instruccion_memoria(conexion_memoria); // esta funcion tiene el nombre de recibir_instruccion_cpu
    }
 
    void manejar_operacion_io(t_instruccion* instruccion, char* interfazIO){
@@ -84,4 +107,15 @@ validar_conexion_IO(char* interfazIO){
     //asumo que vamos a hacer una lista de las interfaces que se van conectando al kernel
     
 
+}
+
+
+void cambiar_a_ready(t_pcb* pcb){
+    pcb_actualizado->estado=READY;
+    queue_push(colaReady, pcb);
+}
+
+void cambiar_a_exit(t_pcb* pcb){
+    pcb_actualizado->estado=EXIT;
+    queue_push(colaExit, pcb);
 }
