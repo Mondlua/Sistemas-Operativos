2315f71c426acf08deb9d074e505c990796e7036
ZoeDominguez <zdominguezcarballo@frba.utn.edu.ar>
2024-07-24 01:18:54 +0000

    SE MUEVEN JOYA LOS DATOS \(*.*)/

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -32,19 +32,23 @@ int main(void) {
 	/* I/O - Cliente */
 
     interfaz = config_get_string_value(entradasalida_config, "TIPO_INTERFAZ");
-    ip_kernel= config_get_string_value(entradasalida_config,"IP_KERNEL");
-    puerto_kernel = config_get_string_value(entradasalida_config, "PUERTO_KERNEL");
+    /*ip_kernel= config_get_string_value(entradasalida_config,"IP_KERNEL");
+    puerto_kernel = config_get_string_value(entradasalida_config, "PUERTO_KERNEL");*/
     extraer_segun_tipo_io(entradasalida_config, interfaz);
-    conexion_kernel = crear_conexion(ip_kernel, puerto_kernel);
+
+    truncar_archivo("archivo2.txt", 70, 0);
+    
+
+    /*conexion_kernel = crear_conexion(ip_kernel, puerto_kernel);
     log_info(entradasalida_log, "I/O conectado a KERNEL");
     send_handshake(conexion_kernel, entradasalida_log, "I/O / KERNEL");
-
+    
     aviso_segun_cod_op(nombre_interfaz, conexion_kernel, INTERFAZ);
 
     pthread_t hilo;
     pthread_create(&hilo, NULL, (void *)recibir_instruccion, interfaz);
     pthread_join(hilo, NULL);
-    
+    */
     return 0;
 }
 
@@ -64,7 +68,7 @@ void extraer_segun_tipo_io(t_config* config, char* tipo_interfaz){
         block_size = config_get_int_value(config, "BLOCK_SIZE");
         block_count = config_get_int_value(config, "BLOCK_COUNT");
         retraso_compactacion = config_get_int_value(config, "RETRASO_COMPACTACION");
-        conectar_con_memoria(config);
+        //conectar_con_memoria(config);
         inicio_filesystem();
     }
 }
